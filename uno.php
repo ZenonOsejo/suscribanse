2d2be8edd9aa15022748a4acacf17d7f173622fe
Zenón Osejo <106634048+ZenonOsejo@users.noreply.github.com>
2022-05-31 19:50:03 -0500

    Add files via upload

---
+++ uno.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>UICSLP Tancanhuitz</title>
+  <link rel="shortcut icon" href="rec/icono2.jpg">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Estilos Css -->
+    <link rel="stylesheet" href="css/uno.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css?family=Red+Hat+Text:400,500,700&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js"></script>
+
+</head>
+<body>
+
+  <!-- Ir Arriba -->
+  <div class="go-top">
+    <span class="fas fa-angle-up"></span>
+  </div>
+
+    <!-- Menu de Navegacion -->
+    <header id="header">
+    <nav class="menu">
+     <div class="logo-box">
+       <h1><a href="index.html">UICSLP Tancanhuitz</a></h1>
+       <span class="btn-menu"><i class="fas fa-bars"></i></span>
+     </div>
+
+     <div class="list-container">
+        <ul class="lists">
+          <li><a href="index.html">Inicio</a></li>
+          <li><a class="active" href="uno.html">Registrate</a></li>
+          <li><a href="dos.html">Oferta Educativa</a></li>
+          <li><a href="tres.html">Comunidad</a></li>
+          <li><a href="cuatro.html">Contactanos</a></li>
+          <li><a href="cinco.html">¿Quienes somos?</a></li>
+        </ul>
+     </div>
+    </nav>
+
+    <!-- Imagen Header -->
+    <div class="img-header">
+     <div class="welcome">
+
+       <h1>Registrate</h1>
+       <hr>
+       <p>Complete el siguiente formulario y nos pondremos en contacto contigo.</p>
+       <button id="abajo">Ver abajo</button>
+     </div>
+    </div>
+
+    <div class="skew-abajo"></div>
+
+    </header>
+
+ <main>
+
+    <!-- Acerca de nosotros -->
+
+
+   <!-- Nuestros proyectos -->
+
+
+   <!-- Testimonios -->
+
+
+ </main>
+
+ <!-- Footer -->
+ <footer class="footer">
+   <div class="skew-arriba"></div>
+  <div class="deg-footer"></div>
+  <div class="ejeZfooter">
+  <div class="footer-content">
+   <div class="footer-title">
+    <h2>--Directorio--</h2>
+  </div>
+  <div class="container">
+   <input type="radio" name="dot" id="one">
+   <input type="radio" name="dot" id="two">
+   <div class="main-card">
+     <div class="cards">
+
+       <div class="card">
+        <div class="content">
+          <div class="img">
+            <img src="images/ANDRES1.jpeg" alt="">
+          </div>
+          <div class="details">
+            <div class="name">M. EN E. ANDRES CRUZ CRUZ</div>
+            <div class="job">Director De La Unidad Académica </div>
+          </div>
+          <div class="media-icons">
+           <a href="direccion.tantz@uicslp.edu.mx"><i class="fa fa-envelope"></i></a>
+            
+          </div>
+        </div>
+       </div>
+
+       <div class="card">
+        <div class="content">
+          <div class="img">
+            <img src="images/ADRIAN.JPEG" alt="">
+          </div>
+          <div class="details">
+            <div class="name">MAE. ADRIAN GARCIA REYES</div>
+            <div class="job">Subdirector Académico</div>
+          </div>
+          <div class="media-icons">
+            
+
+          </div>
+        </div>
+       </div>
+
+       <div class="card">
+         <div class="content">
+           <div class="img">
+             <img src="images/inikal a pau.jpeg" alt="">
+           </div>
+           <div class="details">
+             <div class="name">C.P JUAN REYES JERNANDEZ</div>
+             <div class="job">Subdirector Escolar</div>
+           </div>
+           <div class="media-icons">
+             <a href="direccion.tantz@uicslp.edu.mx"><i class="fa fa-envelope"></i></a>
+             
+
+           </div>
+         </div>
+        </div>
+
+       <div class="card">
+        <div class="content">
+          <div class="img">
+            <img src="images/josue.jpeg" alt="">
+          </div>
+          <div class="details">
+            <div class="name">LIC. JOSUE AGUILAR LOPEZ</div>
+            <div class="job">Subdirección Administrativa</div>
+          </div>
+          <div class="media-icons">
+           <a href="direccion.tantz@uicslp.edu.mx"><i class="fa fa-envelope"></i></a>
+            
+          </div>
+        </div>
+       </div>
+     </div>
+
+     <div class="cards">
+       <div class="card">
+        <div class="content">
+         <div class="img">
+           <img src="images/ANDRES1.jpeg" alt="">
+         </div>
+         <div class="details">
+           <div class="name">M. EN E. ANDRES CRUZ CRUZ</div>
+           <div class="job">Director De La Unidad Académica </div>
+         </div>
+          <div class="media-icons">
+           <a href="direccion.tantz@uicslp.edu.mx"><i class="fa fa-envelope"></i></a>
+            
+          </div>
+        </div>
+       </div>
+       
+       <div class="card">
+         <div class="content">
+           <div class="img">
+             <img src="images/ADRIAN.JPEG" alt="">
+           </div>
+           <div class="details">
+             <div class="name">ADRIAN GARCIA REYES</div>
+             <div class="job">Subdirector Académico</div>
+           </div>
+           <div class="media-icons">
+             
+            
+           </div>
+         </div>
+        </div>
+        <div class="card">
+         <div class="content">
+           <div class="img">
+             <img src="images/inikal a pau.jpeg" alt="">
+           </div>
+           <div class="details">
+             <div class="name">C.P JUAN REYES JERNANDEZ</div>
+             <div class="job">Subdirector Escolar</div>
+           </div>
+           <div class="media-icons">
+             <a href="direccion.tantz@uicslp.edu.mx"><i class="fa fa-envelope"></i></a>
+             
+
+           </div>
+         </div>
+        </div>
+
+        <div class="card">
+         <div class="content">
+           <div class="img">
+             <img src="images/josue.jpeg" alt="">
+           </div>
+           <div class="details">
+             <div class="name">LIC. JOSUE AGUILAR LOPEZ</div>
+             <div class="job">Subdirección Administrativa</div>
+           </div>
+           <div class="media-icons">
+             <a href="direccion.tantz@uicslp.edu.mx"><i class="fa fa-envelope"></i></a>
+            
+           </div>
+         </div>
+        </div>
+
+ 
+     </div>
+   </div>
+    <div class="button">
+      <label for="one" class=" active one"></label>
+      <label for="two" class="two"></label>
+    </div>
+  </div>
+
+   <div class="sm">
+    <a href="https://www.facebook.com/Universidad-Intercultural-de-San-Luis-Potos%C3%AD-1434950560080820/"><i class='bx bxl-facebook'></i></a>
+    <a href="#"><i class='bx bxl-gmail'></i></a>
+    <a href="https://www.instagram.com/interculturalsanluispotosi/?igshid=1wimkulw79kow/"><i class="fab fa-instagram"></i></a>
+    <a href="https://www.youtube.com/channel/UC9xAAklgGTu1OCea3Ao3lRA"><i class="fab fa-youtube"></i></a>
+
+  </div>
+
+   <div class="footer-text">
+     <p>&copy; UICSLP | Unidad Académica de Tancanhuitz</p>
+   </div>
+
+  </div>
+</div>
+ </footer>
+
+<!-- Scripts -->
+<script src="https://kit.fontawesome.com/35db202371.js"></script>
+<script src="js/appe.js"></script>
+
+</body>
+</html>
